791733ab7eb3ff58c29a61ac5bfac31339028f84
gruand <gruand@gmail.com>
2024-03-05 12:51:18 +0200

    Add green frame around new object in the initial frame

--- main.py
+++ main.py
@@ -12,19 +12,34 @@ while True:
     check, frame = video.read()
     # Convert frame to grey to decrease volume of data
     grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     # Add Gaussian Blur
     grey_frame_gau = cv2.GaussianBlur(grey_frame, (21, 21), 0)
-    # cv2.imshow("My Video", grey_frame_gau)
 
     if first_frame is None:
         first_frame = grey_frame_gau
 
+    # Delta with first frame
     delta_frame = cv2.absdiff(first_frame, grey_frame_gau)
+
+    # Threshold of delta to make high contrast of pixels
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    cv2.imshow("Video from Camera. Press Q to Exit", thresh_frame)
 
+    # Add dilate to threshold
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    # cv2.imshow("Video from Camera. Press Q to Exit", dil_frame)
+    # Find contours of diff image
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Video from Camera. Press Q to Exit", frame)
     key = cv2.waitKey(1)
-
+    # Exit if user press "q" button
     if key == ord('q'):
         break
 
